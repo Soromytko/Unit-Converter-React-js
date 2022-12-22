de1a96fdebb9e98f543b57a3098bdd8897085c6b
Soromytko <NikitaSoromytko@gmail.com>
2022-12-22 20:14:45 +0300

    add basic functionality

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//ReactDOM.render(<h1>Hello</h1>, document.getElementById("app"))
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <dev>
+        Приложение работает
+    </dev>,
+    document.getElementById('app')
+);
+
