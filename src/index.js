92687cce0b547d1a008b550d44f88522cd362002
Soromytko <NikitaSoromytko@gmail.com>
2022-12-26 01:08:59 +0300

    basic functionality

--- src/index.js
+++ src/index.js
@@ -1,64 +1,153 @@
-'use strict';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const e = React.createElement;
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
+class DownDropList extends React.Component {
+    constructor (props) {
+        super(props)
     }
 
-    return e(
-      'button',
-      { 
-        onClick: () => this.setState({ liked: true })
-      },
-      'Like'
-    );
-  }
+    render () {
+        return (
+            <button>{this.props.value}</button>
+        )
+    }
 }
 
-class ShoppingList extends React.Component {
-  render() {
-    return (
-      <div className="shopping-list">
-        <h1>Shopping List for {this.props.name}</h1>
-        <ul>
-          <li>Instagram</li>
-          <li>WhatsApp</li>
-          <li>Oculus</li>
-        </ul>
-      </div>
-    );
-  }
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const  centerStryle = {
+            display: 'table',
+            margin: '0 auto',
+            border: '1px solid black',
+        };
+
+        const  innerStyle = {
+            display: 'table',
+            verticalAlign: 'top',
+            horizontalAligment: 'right',
+            margin: '0 auto',
+            border: '1px solid black',
+            width: '40%',
+        };
+
+        const  rightStyle = {
+            display: 'table',
+            verticalAlign: 'top',
+            margin: '0 auto',
+            border: '1px solid black',
+            width: '40%',
+        };
+
+        // const outerStyle = {
+        //     border: '1px solid red',
+        //     marginTop: '200px',
+        //     width: '100%',
+        // }
+
+        return (
+            <div>
+                <div>
+                    <DownDropList value={"Button1"}>Button1</DownDropList>
+                    <DownDropList value={'Button1'}>Button2</DownDropList>
+                </div>
+            </div>
+            
+        )
+    }
 }
 
-class ShoppingListLinux extends React.Component {
-  constructor(props) {
-    super(props);
+class Fr extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: 'coconut'};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('Your favorite flavor is: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Pick your favorite flavor:
+            <select value={this.state.value} onChange={this.handleChange}>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
   }
 
-  render() {
-    return React.createElement("div", {
-      className: "shopping-list"
-    },
-    React.createElement("h1", null, "Shopping List for ", "props.name"),
-    React.createElement("ul", null, 
-    React.createElement("li", null, "Instagram"), 
-    React.createElement("li", null, "WhatsApp"), 
-    React.createElement("li", null, "Oculus")));
+
+  class NameForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
   }
-}
 
-//ON LINUX
-// const domContainer = document.querySelector('#root');
-// const root = ReactDOM.createRoot(domContainer);
-// root.render(e(LikeButton));
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<LikeButton />);
-root.render(e(ShoppingListLinux))
+
+ReactDOM.render(<App/>, document.querySelector("#root"))
+
+// import * as d3 from "d3"
+// import * as Geo from "./geo.json"
+
+// import {React} from 'react'
+// import ReactDOM from 'react-dom/client'
+// import {App} from "./components/app";
+
+// document.addEventListener("DOMContentLoaded", setup)
+
+// function setup(){
+//     const root = ReactDOM.createRoot(document.getElementById('root'));
+//     const element = <App />;
+//     root.render(element);
+
+// }
+
+
+
