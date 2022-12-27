06334e417868ba19cedf77fae0a8da20abc781a9
Soromytko <NikitaSoromytko@gmail.com>
2022-12-27 05:42:14 +0300

    basic functionality

--- src/index.js
+++ src/index.js
@@ -1,99 +1,135 @@
+import { react } from "@babel/types";
+import { shouldInstrument } from "@jest/transform";
 import { thresholdFreedmanDiaconis } from "d3";
 import React from "react";
 import ReactDOM from "react-dom";
 
-let items = [];
-for(var i = 0; i < 3; i++) {
-  items.push(<option value="lime">Lime</option>);
+var units = [];
+units['см'] = 0.01;
+units['м'] = 1;
+units['км'] = 1000;
+
+function render() {
+  let result = []
+  for(const [key, value] of Object.entries(units)) {
+    result.push(<option key={key} value={key}>{key}</option>)
+  }
+  return (
+    <select onChange={this.handleChange}>
+      {result}
+    </select>
+  )
 }
 
+function conver(value, fromUnit, toUnit) {
+  return value * units[fromUnit] / units[toUnit]
+}
 
-class DownDropList extends React.Component {
-    constructor (props) {
-        super(props)
+class DropDownList extends React.Component {
+  constructor(props) {
+    super(props)
 
-        this.items = [];
+    this.items = []
+    this.handleChange = this.onValueChanged.bind(this)
+  }
+
+  onValueChanged(event) {
+    console.log('onValueChanged', event.target.value)
+  }
+
+  addItem(name, value) {
+    this.items[name] = value;
+  }
+
+  render() {
+    let result = []
+    for(const [key, value] of Object.entries(units)) {
+      result.push(<option key={key} value={key}>{key}</option>)
     }
+    return (
+      <select onChange={this.handleChange}>
+        {result}
+      </select>
+    )
+  }
+}
 
-    addItem(name, value) {
-      this.items.key = value;
-    //   this.items.push({
-    //     key: name,
-    //     value: value
-    // });
-    }
+class FieldReact extends React.Component {
+  constructor(props) {
+    super(props);
 
-    getReactItems() {
-      let reactItems = []
-      for(const [key, value] of Object.entries(this.items)) {
-        reactItems.push(<option value={key}>{key}</option>);
-      }
-      return 
-    }
+    this.handleChange = this.onHandleChange.bind(this)
+    this.dropDownList = new DropDownList();
+  }
 
-    render () {
-        return (
-            <select>
-              {/* <option value="grapefruit">Grapefruit</option>
-              <option value="lime">Lime</option>
-              <option value="coconut">Coconut</option>
-              <option value="mango">Mango</option> */}
-              {items} 
-            </select>
-            //<button>{this.props.value}</button>
-        )
-    }
+  onHandleChange(event) {
+    console.log(event.target.value)
+  }
+
+  render () {
+    return (
+          <label>
+            <input id={this.id} type="text" onChange={this.handleChange}></input>
+            {this.dropDownList.render()}
+          </label>
+    )
+  }
 }
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.fromList = new DownDropList();
-        for (let i = 0; i < 3; i++) {
-          this.fromList.addItem("sd", i);
-        }
-         
+        // this.fromDropDownList = new DropDownList();
+        // this.toDropDownList = new DropDownList();
+
+        // this.fromDropDownList.handleChange = this.onC.bind(this);
+
+        // for (let i = 0; i < 3; i++) {
+        //   this.fromDropDownList.addItem("item" + i, i);
+        //   this.toDropDownList.addItem("item" + i, i);
+        // }
+
+        this.fromField = new FieldReact()
+        this.fromField.id = "fromInput"
+        this.fromField.handleChange = this.onFromInputFieldChanged.bind(this)
+        this.fromField.dropDownList.handleChange = this.onFromDropListHandleCnahged.bind(this)
+
+        this.toField = new FieldReact()
+        this.toField.id = "toInput"
+        this.toField.handleChange = this.onToInputFieldChanged.bind(this)
+        this.toField.dropDownList.handleChange = this.onToDropListHandleCnahged.bind(this)
+
+    }
+
+    onFromInputFieldChanged(event) {
+      var value = Number(event.target.value)
+      if(value == NaN) return
+      // console.log(this.toField)
+      var newValue = conver(value, 'км', 'м')
+      //var input = document.getElementById("toInput");
+      //setNativeValue(input, newValue);
+    }
+
+    onFromDropListHandleCnahged(event) {
+      console.log("from value", event.target.value);
+    }
+
+    onToInputFieldChanged(event) {
+      console.log("to inputField changed", event.target.value)
+    }
+
+    onToDropListHandleCnahged(event) {
+      console.log("to value", event.target.value);
     }
 
     render() {
-        const  centerStryle = {
-            display: 'table',
-            margin: '0 auto',
-            border: '1px solid black',
-        };
-
-        const  innerStyle = {
-            display: 'table',
-            verticalAlign: 'top',
-            horizontalAligment: 'right',
-            margin: '0 auto',
-            border: '1px solid black',
-            width: '40%',
-        };
-
-        const  rightStyle = {
-            display: 'table',
-            verticalAlign: 'top',
-            margin: '0 auto',
-            border: '1px solid black',
-            width: '40%',
-        };
-
-        // const outerStyle = {
-        //     border: '1px solid red',
-        //     marginTop: '200px',
-        //     width: '100%',
-        // }
-
         return (
             <div>
-                <div>
-                    {/* <DownDropList value={"Button1"}>Button1</DownDropList>
-                    <DownDropList value={'Button1'}>Button2</DownDropList> */}
-                </div>
+              {this.fromField.render()}
+              <label> = </label>
+              {this.toField.render()}
             </div>
-            
         )
     }
 }
@@ -134,7 +170,6 @@ class Fr extends React.Component {
     }
   }
 
-
   class NameForm extends React.Component {
     constructor(props) {
       super(props);
@@ -167,24 +202,26 @@ class Fr extends React.Component {
   }
 
 
+  //https://stackoverflow.com/questions/30683628/react-js-setting-value-of-input
+  function setNativeValue(element, value) {
+    let lastValue = element.value;
+    element.value = value;
+    let event = new Event("input", { target: element, bubbles: true });
+    // React 15
+    event.simulated = true;
+    // React 16
+    let tracker = element._valueTracker;
+    if (tracker) {
+        tracker.setValue(lastValue);
+    }
+    element.dispatchEvent(event);
+}
+//var input = document.getElementById("ID OF ELEMENT");
+//setNativeValue(input, "VALUE YOU WANT TO SET");
+
 
 ReactDOM.render(<App/>, document.querySelector("#root"))
 
-// import * as d3 from "d3"
-// import * as Geo from "./geo.json"
-
-// import {React} from 'react'
-// import ReactDOM from 'react-dom/client'
-// import {App} from "./components/app";
-
-// document.addEventListener("DOMContentLoaded", setup)
-
-// function setup(){
-//     const root = ReactDOM.createRoot(document.getElementById('root'));
-//     const element = <App />;
-//     root.render(element);
-
-// }
 
 
 
